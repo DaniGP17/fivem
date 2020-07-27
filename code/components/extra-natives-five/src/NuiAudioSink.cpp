DaniGP17/fivem

commit 90db8a077c3129a904857bfd1cd1328bdef21378

feature: merged commit for multi-build support for 1.0.1868.0

code/components/extra-natives-five/src/NuiAudioSink.cpp
@@ -191,12 +191,13 @@ namespace rage
 		return hook::get_pattern("0F 57 C0 48 8D 41 10 BA 03 00 00 00");
 	});
 
+	static uint32_t* initParamVal;
+
 	audSoundInitParams::audSoundInitParams()
 	{
 		_audSoundInitParams_ctor(this);
 
-		// 1604
-		m_pad[0x9A] = *(uint32_t*)hook::get_adjusted(0x141C5F998);
+		m_pad[0x9A] = *initParamVal;
 	}
 
 	class audEntity
@@ -238,6 +239,8 @@ namespace rage
 		g_frontendAudioEntity = hook::get_address<audEntity*>(hook::get_pattern("4C 8D 4C 24 50 4C 8D 43 08 48 8D 0D", 0xC));
 
 		g_categoryMgr = hook::get_address<audCategoryManager*>(hook::get_pattern("48 8B CB BA EA 75 96 D5 E8", -4));
+
+		initParamVal = hook::get_address<uint32_t*>(hook::get_pattern("BA 11 CC 23 C3 E8 ? ? ? ? 48 8D", 0x16b));
 	});
 
 	static hook::cdecl_stub<audWaveSlot*(uint32_t)> _findWaveSlot([]()

code/client/launcher/CitizenGame.cpp
@@ -135,51 +135,6 @@ int ReturnInt()
 	return Value;
 }
 
-static int CustomGameElementCall(char* element)
-{
-	static std::map<uint32_t, std::string> hashMap;
-
-	if (hashMap.size() == 0)
-	{
-		FILE* f = fopen("Y:\\dev\\v\\strings2.txt", "r");
-
-		if (f)
-		{
-			char stringBuf[4096];
-
-			while (!feof(f))
-			{
-				fgets(stringBuf, sizeof(stringBuf), f);
-				stringBuf[sizeof(stringBuf) - 1] = '\0';
-
-				stringBuf[strlen(stringBuf) - 2] = '\0';
-
-				hashMap[HashString(stringBuf)] = stringBuf;
-			}
-		}
-	}
-
-	uint32_t hash = *(uint32_t*)(element + 16);
-
-	std::string name;
-
-	auto it = hashMap.find(hash);
-
-	if (it != hashMap.end())
-	{
-		name = " - " + it->second;
-	}
-
-	trace("Entered game element %08x%s.\n", hash, name.c_str());
-
-	uintptr_t func = *(uintptr_t*)(element + 32);
-	int retval = ((int(*)())func)();
-
-	trace("Exited game element %08x%s.\n", hash, name.c_str());
-
-	return retval;
-}
-
 static void* DeleteVideo(void*, char* videoName)
 {
 	strcpy(videoName, "nah");
@@ -210,7 +165,9 @@ VOID WINAPI GetStartupInfoWHook(_Out_ LPSTARTUPINFOW lpStartupInfo)
 	}
 
 	// ignore launcher requirement
-	hook::call(hook::pattern("E8 ? ? ? ? 84 C0 75 ? B2 01 B9 2F A9 C2 F4").count(1).get(0).get<void>(), ThisIsActuallyLaunchery);
+	// 1737 updated this for MTL
+	// and 1868 made it Arxan.
+	hook::call(hook::pattern("84 C0 75 0C B2 01 B9 2F A9 C2 F4").count(1).get(0).get<void>(-5), ThisIsActuallyLaunchery);
 
 	// ignore steam requirement
 	/*auto pattern = hook::pattern("FF 15 ? ? ? ? 84 C0 74 0C B2 01 B9 91 32 25");// 31 E8");
@@ -225,12 +182,6 @@ VOID WINAPI GetStartupInfoWHook(_Out_ LPSTARTUPINFOW lpStartupInfo)
 	
 	// draw loading screen even if 'not' enabled
 	hook::nop(hook::get_pattern("0F 29 74 24 30 85 DB", 7), 6);
-
-	// game elements for crash handling purposes
-	char* vtablePtrLoc = hook::pattern("41 89 40 10 49 83 60 18 00 48 8D 05").count(1).get(0).get<char>(12);
-	void* vtablePtr = (void*)(*(int32_t*)vtablePtrLoc + vtablePtrLoc + 4);
-
-	//hook::put(&((uintptr_t*)vtablePtr)[1], CustomGameElementCall);
 
 	if (!g_launcher->PostLoadGame(GetModuleHandle(nullptr), nullptr))
 	{

code/client/launcher/ExecutableLoader.Snapshot.cpp
@@ -3,9 +3,12 @@
 
 #include <Hooking.h>
 
-// 1604 now...!
+#include <CrossBuildRuntime.h>
+
 #ifdef GTA_FIVE
-#define TRIGGER_EP 0x14175DE00
+// 1604
+// 1868 now...!
+#define TRIGGER_EP ((Is1868()) ? 0x141780F74 : 0x14175DE00)
 #elif defined(IS_RDR3)
 // 1207.58
 //#define TRIGGER_EP 0x142D55C2C

code/client/launcher/GameCache.cpp
@@ -134,11 +134,12 @@ struct GameCacheStorageEntry
 };
 
 // global cache mapping of ROS files to disk files
-static GameCacheEntry g_requiredEntries[] =
+static std::vector<GameCacheEntry> g_requiredEntries =
 {
 #if defined(GTA_FIVE)
-	{ "GTA5.exe", "8939c8c71aa98ad7ca6ac773fae1463763c420d8", "https://runtime.fivem.net/patches/GTA_V_Patch_1_0_1604_0.exe", "$/GTA5.exe", 72484280, 1031302600 },
-	{ "update/update.rpf", "fc941d698834e30e40a06a40f6a35b1b18e1c50c", "https://runtime.fivem.net/patches/GTA_V_Patch_1_0_1604_0.exe", "$/update/update.rpf", 966805504, 1031302600 },
+	//{ "GTA5.exe", "79a272830be65afae4acd520bfbfe176e0b142f3", "https://runtime.fivem.net/patches/GTA_V_Patch_1_0_1737_0.exe", "$/GTA5.exe", 77631632, 1189307336 },
+	//{ "update/update.rpf", "8a98e0879b91661dd2aa23f86abdf91ee741b237", "https://runtime.fivem.net/patches/GTA_V_Patch_1_0_1737_0.exe", "$/update/update.rpf", 1171902464, 1189307336 },
+
 	//{ L"update/update.rpf", "c819ecc1df08f3a90bc144fce0bba08bb7b6f893", "nope:https://runtime.fivem.net/patches/dlcpacks/patchday4ng/dlc.rpfupdate.rpf", 560553984 },
 	//{ "update/update.rpf", "319d867a44746885427d9c40262e9d735cd2a169", "Game_EFIGS/GTA_V_Patch_1_0_1011_1.exe", "$/update/update.rpf", 701820928, SIZE_MAX },
 	{ "update/x64/dlcpacks/patchday4ng/dlc.rpf", "124c908d82724258a5721535c87f1b8e5c6d8e57", "nope:https://runtime.fivem.net/patches/dlcpacks/patchday4ng/dlc.rpfpatchday4ng/dlc.rpf", 312438784 },
@@ -1027,8 +1028,24 @@ static bool PerformUpdate(const std::vector<GameCacheEntry>& entries)
 	return true;
 }
 
+#include <CrossBuildRuntime.h>
+
 std::map<std::string, std::string> UpdateGameCache()
 {
+	// 1604/1868 toggle
+#ifdef GTA_FIVE
+	if (!Is1868())
+	{
+		g_requiredEntries.push_back({ "GTA5.exe", "8939c8c71aa98ad7ca6ac773fae1463763c420d8", "https://runtime.fivem.net/patches/GTA_V_Patch_1_0_1604_0.exe", "$/GTA5.exe", 72484280, 1031302600 });
+		g_requiredEntries.push_back({ "update/update.rpf", "fc941d698834e30e40a06a40f6a35b1b18e1c50c", "https://runtime.fivem.net/patches/GTA_V_Patch_1_0_1604_0.exe", "$/update/update.rpf", 966805504, 1031302600 });
+	}
+	else
+	{
+		g_requiredEntries.push_back({ "GTA5.exe", "79a272830be65afae4acd520bfbfe176e0b142f3", "https://mirrors.fivem.net/patches/1868.0/GTA5.exe", 77631632 });
+		g_requiredEntries.push_back({ "update/update.rpf", "8a98e0879b91661dd2aa23f86abdf91ee741b237", "https://mirrors.fivem.net/patches/1868.0/update.rpf", 1171902464 });
+	}
+#endif
+
 	// delete bad migration on 2019-01-10 (incorrect update.rpf download URL caused Steam users to fetch 1493.1 instead of 1604.0)
 	{
 		auto dataPath = MakeRelativeCitPath(L"cache\\game\\cache.dat");

code/client/launcher/Main.cpp
@@ -20,6 +20,8 @@
 
 #include <shellscalingapi.h>
 
+#include <CrossBuildRuntime.h>
+
 #include <shobjidl.h>
 
 extern "C" BOOL WINAPI _CRT_INIT(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved);
@@ -198,6 +200,27 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
 	SetCurrentProcessExplicitAppUserModelID(L"CitizenFX." PRODUCT_NAME L".Client");
 
+	// toggle wait for switch
+	if (wcsstr(GetCommandLineW(), L"-switchcl"))
+	{
+		// if this isn't a subprocess
+		wchar_t fxApplicationName[MAX_PATH];
+		GetModuleFileName(GetModuleHandle(nullptr), fxApplicationName, _countof(fxApplicationName));
+
+		if (wcsstr(fxApplicationName, L"subprocess") == nullptr)
+		{
+			static HostSharedData<CfxState> initStateOld("CfxInitState");
+
+			HANDLE hProcess = OpenProcess(SYNCHRONIZE, FALSE, initStateOld->initialGamePid);
+
+			if (hProcess)
+			{
+				WaitForSingleObject(hProcess, INFINITE);
+				CloseHandle(hProcess);
+			}
+		}
+	}
+
 	static HostSharedData<CfxState> initState("CfxInitState");
 
 #ifdef IS_LAUNCHER
@@ -312,7 +335,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 				if (AssignProcessToJobObject(hJob, GetCurrentProcess()))
 				{
 					JOBOBJECT_EXTENDED_LIMIT_INFORMATION info = {};
-					info.BasicLimitInformation.LimitFlags = JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE;
+					info.BasicLimitInformation.LimitFlags = JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE | JOB_OBJECT_LIMIT_BREAKAWAY_OK;
 					if (SetInformationJobObject(hJob, JobObjectExtendedLimitInformation, &info, sizeof(info)))
 					{
 						initState->inJobObject = true;
@@ -467,6 +490,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 	if (initState->IsMasterProcess())
 	{
 		NVSP_DisableOnStartup();
+
+		GetModuleFileNameW(NULL, initState->gameExePath, std::size(initState->gameExePath));
 	}
 
 	// readd the game path into the PATH
@@ -531,9 +556,9 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
 				VS_FIXEDFILEINFO* fixedInfo = reinterpret_cast<VS_FIXEDFILEINFO*>(fixedInfoBuffer);
 				
-				if ((fixedInfo->dwFileVersionLS >> 16) != 1604)
+				if ((fixedInfo->dwFileVersionLS >> 16) != ((Is1868()) ? 1868 : 1604))
 				{
-					MessageBox(nullptr, va(L"The found GTA executable (%s) has version %d.%d.%d.%d, but only 1.0.1604.0 is currently supported. Please obtain this version, and try again.",
+					MessageBox(nullptr, va(L"The found GTA executable (%s) has version %d.%d.%d.%d, but only 1.0.1604.0/1.0.1868.0 is currently supported. Please obtain this version, and try again.",
 										   gameExecutable.c_str(),
 										   (fixedInfo->dwFileVersionMS >> 16),
 										   (fixedInfo->dwFileVersionMS & 0xFFFF),
@@ -574,7 +599,14 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 				UI_UpdateText(0, va(L"Starting %s...", 
 					ToWide(minModeManifest->Get("productName", ToNarrow(PRODUCT_NAME)))));
 
-				UI_UpdateText(1, ToWide(minModeManifest->Get("productSubtitle", "We're getting there.")).c_str());
+				if (wcsstr(GetCommandLineW(), L"-switchcl"))
+				{
+					UI_UpdateText(1, L"Transitioning to another build...");
+				}
+				else
+				{
+					UI_UpdateText(1, ToWide(minModeManifest->Get("productSubtitle", "We're getting there.")).c_str());
+				}
 
 				while (GetTickCount64() < (st + 3500))
 				{

code/client/shared/CfxState.h
@@ -16,10 +16,13 @@ struct CfxState
 	wchar_t initPathGame[1024];
 	wchar_t initPathLauncher[1024];
 
+	wchar_t gameExePath[1024];
+
 	CfxState()
 	{
 		memset(initPathGame, 0, sizeof(initPathGame));
 		memset(initPathLauncher, 0, sizeof(initPathLauncher));
+		memset(gameExePath, 0, sizeof(gameExePath));
 
 		initialGamePid = 0;
 		initialLauncherPid = 0;

code/client/shared/CfxSubProcess.h
@@ -36,13 +36,16 @@ inline const wchar_t* MakeCfxSubProcess(const std::wstring& processType)
 #ifndef IS_FXSERVER
 	if (wcsstr(GetCommandLine(), L"cl2") != nullptr)
 	{
-		outPath += productName + L"cl2_" + processType;
+		productName += L"cl2_";
 	}
-	else
-#endif
+	
+	if (wcsstr(GetCommandLine(), L"b1868") != nullptr)
 	{
-		outPath += productName + processType;
+		productName += L"b1868_";
 	}
+#endif
+
+	outPath += productName + processType;
 
 	DeleteFile(outPath.c_str());
 	CopyFile(fxApplicationName, outPath.c_str(), FALSE);

code/client/shared/CrossBuildRuntime.h
@@ -1,0 +1,13 @@
+#pragma once
+
+inline bool Is1868()
+{
+#ifdef GTA_FIVE
+	if (wcsstr(GetCommandLine(), L"b1868") != nullptr)
+	{
+		return true;
+	}
+#endif
+
+	return false;
+}

commit b29e83ecac8c537333804efd8a1cb97587d12c85

fix(audio): use correct initParamVal for audSoundInitParams
This seemignly broke in 90db8a077c3129a904857bfd1cd1328bdef21378 - a random `b` appeared, breaking creation of some sound instances.

code/components/extra-natives-five/src/NuiAudioSink.cpp
@@ -300,7 +300,7 @@ namespace rage
 		return hook::get_pattern("0F 57 C0 48 8D 41 10 BA 03 00 00 00");
 	});
 
-	static uint32_t* initParamVal;
+	static uint8_t* initParamVal;
 
 	audSoundInitParams::audSoundInitParams()
 	{
@@ -539,7 +539,7 @@ namespace rage
 
 		g_categoryMgr = hook::get_address<audCategoryManager*>(hook::get_pattern("48 8B CB BA EA 75 96 D5 E8", -4));
 
-		initParamVal = hook::get_address<uint32_t*>(hook::get_pattern("BA 11 CC 23 C3 E8 ? ? ? ? 48 8D", 0x16b));
+		initParamVal = hook::get_address<uint8_t*>(hook::get_pattern("BA 11 CC 23 C3 E8 ? ? ? ? 48 8D", 0x16));
 	});
 
 	static hook::cdecl_stub<audWaveSlot*(uint32_t)> _findWaveSlot([]()