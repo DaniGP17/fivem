DaniGP17/fivem

commit 6b9bcdd7fdb66a499d1f954c5fa268351cfea43c

fix(onesync/client): calibrate distance culling range for distant scene update
At the expense of reducing its effect on bigmode, this range should match the usual range used on the server for population-spawned entities so that vehicles will not be in an inconsistent state when migrating (e.g. affected by physics but not re-blended from the network sync node, leading to near-fallen helicopters).

code/components/gta-net-five/src/CloneManager.cpp
@@ -1673,7 +1673,7 @@ void CloneManagerLocal::Update()
 
 						auto it = removedFlags.find(ent);
 
-						if (DirectX::XMVectorGetX(DirectX::XMVector3LengthSq(DirectX::XMVectorSubtract(pos, origin))) < (300.f * 300.f))
+						if (DirectX::XMVectorGetX(DirectX::XMVector2LengthSq(DirectX::XMVectorSubtract(pos, origin))) < (424.f * 4240.f))
 						{
 							if (it != removedFlags.end())
 							{

commit 47f2013ef3731ce1a6dc06d043c26224da07a882

fix(onesync/client): typo introduced in 6b9bcdd7fdb66a499d1f954c5fa268351cfea43c

code/components/gta-net-five/src/CloneManager.cpp
@@ -1673,7 +1673,7 @@ void CloneManagerLocal::Update()
 
 						auto it = removedFlags.find(ent);
 
-						if (DirectX::XMVectorGetX(DirectX::XMVector2LengthSq(DirectX::XMVectorSubtract(pos, origin))) < (424.f * 4240.f))
+						if (DirectX::XMVectorGetX(DirectX::XMVector2LengthSq(DirectX::XMVectorSubtract(pos, origin))) < (424.f * 424.f))
 						{
 							if (it != removedFlags.end())
 							{