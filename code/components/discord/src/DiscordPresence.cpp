DaniGP17/fivem

commit 9359e9e5e4d252e5d97d51887c0c43b99d0218e3

feat(five): localGame and storyMode presence data
Requested in the main Discord guild.

Also fixes some backing anomalies in handling presence state.

code/components/discord/src/DiscordPresence.cpp
@@ -70,8 +70,15 @@ static void UpdatePresence()
 			g_richPresenceValues[7]
 		);
 
-		std::string line1 = formattedRichPresence.substr(formattedRichPresence.find_first_of("\n") + 1);
-		std::string line2 = formattedRichPresence.substr(0, formattedRichPresence.find_first_of("\n"));
+		auto lineOff = formattedRichPresence.find_first_of("\n");
+
+		std::string line1 = formattedRichPresence.substr(0, lineOff);
+		std::string line2;
+
+		if (lineOff != std::string::npos)
+		{
+			line2 = formattedRichPresence.substr(lineOff + 1);
+		}
 
 		if (!g_richPresenceOverride.empty())
 		{
@@ -163,18 +170,24 @@ static InitFunction initFunction([]()
 	{
 		g_startTime = time(nullptr);
 
-		g_richPresenceTemplate = text;
+		if (g_richPresenceTemplate != text)
+		{
+			g_richPresenceTemplate = text;
 
-		g_richPresenceChanged = true;
+			g_richPresenceChanged = true;
+		}
 	});
 
 	OnRichPresenceSetValue.Connect([](int idx, const std::string& value)
 	{
 		assert(idx >= 0 && idx < _countof(g_richPresenceValues));
 
-		g_richPresenceValues[idx] = value;
+		if (g_richPresenceValues[idx] != value)
+		{
+			g_richPresenceValues[idx] = value;
 
-		g_richPresenceChanged = true;
+			g_richPresenceChanged = true;
+		}
 	});
 
 	OnRichPresenceSetTemplate("In the menus\n");

code/components/citizen-level-loader-five/src/LevelLoader.cpp
@@ -366,6 +366,7 @@ static InitFunction initFunction([] ()
 	static ConsoleCommand localGameCommand("localGame", [](const std::string& resourceDir)
 	{
 		Instance<ICoreGameInit>::Get()->SetVariable("localMode");
+		Instance<ICoreGameInit>::Get()->SetData("localResource", resourceDir);
 
 		fx::ResourceManager* resourceManager = Instance<fx::ResourceManager>::Get();
 		resourceManager->AddMounter(new SPResourceMounter(resourceManager));

code/components/lovely-script/src/Lovely.cpp
@@ -146,7 +146,48 @@ public:
 #elif defined(IS_RDR3)
 		uint32_t playerPedId = NativeInvoke::Invoke<0x096275889B8E0EE0, uint32_t>();
 #endif
-		
+
+		auto setPresenceTemplate = [](std::string_view value)
+		{
+			static auto steam = GetSteam();
+			std::string valueStr{ value };
+
+			if (steam)
+			{
+				steam->SetRichPresenceTemplate(valueStr);
+			}
+
+			OnRichPresenceSetTemplate(valueStr);
+		};
+
+		auto setPresenceValue = [](int idx, std::string_view value)
+		{
+			static auto steam = GetSteam();
+			std::string valueStr{ value };
+
+			if (steam)
+			{
+				steam->SetRichPresenceValue(idx, valueStr);
+			}
+
+			OnRichPresenceSetValue(idx, valueStr);
+		};
+
+		static auto icgi = Instance<ICoreGameInit>::Get();
+
+		if (icgi->HasVariable("localMode"))
+		{
+			std::string localName = "None?!";
+			icgi->GetData("localResource", &localName);
+
+			setPresenceTemplate("Playing a localGame: {0}");
+			setPresenceValue(0, localName);
+		}
+		else if (icgi->HasVariable("storyMode"))
+		{
+			// #TODO: find a way to fake out scripts' IS_XBOX360_VERSION/.. calls for NETWORK_SET_RICH_PRESENCE et al.
+			setPresenceTemplate("Playing Story Mode");
+		}
 
 		if (playerPedId != -1 && playerPedId != 0)
 		{
@@ -159,7 +200,7 @@ public:
 
 				static auto icgi = Instance<ICoreGameInit>::Get();
 
-				if (!icgi->HasVariable("storyMode"))
+				if (!icgi->HasVariable("storyMode") && !icgi->HasVariable("localMode"))
 				{
 					color = CRGBA(200, 0, 0, 180);
 					TheFonts->DrawRectangle(rect, color);
@@ -170,8 +211,6 @@ public:
 			if (NativeInvoke::Invoke<0x9DE624D2FC4B603F, bool>())
 #endif
 			{
-				auto steam = GetSteam();
-
 				int playerCount = 0;
 
 				for (int i = 0; i < 256; i++)
@@ -187,12 +226,7 @@ public:
 
 				if (playerCount != lastPlayerCount)
 				{
-					if (steam)
-					{
-						steam->SetRichPresenceValue(1, fmt::sprintf("%d player%s", playerCount, (playerCount != 1) ? "s" : ""));
-					}
-
-					OnRichPresenceSetValue(1, fmt::sprintf("%d player%s", playerCount, (playerCount != 1) ? "s" : ""));
+					setPresenceValue(1, fmt::sprintf("%d player%s", playerCount, (playerCount != 1) ? "s" : ""));
 					lastPlayerCount = playerCount;
 				}
 			}

commit a6c466ff14a0d1cb252304fa1cd7315fd478d3f2

tweak(discord): move presence override to line 2
Apparently the change in 9359e9e5e4d252e5d97d51887c0c43b99d0218e3 broke
user expectations by replacing the wrong line from the original data.

Fixes #1362.

code/components/discord/src/DiscordPresence.cpp
@@ -8,7 +8,6 @@
 #include <NetLibrary.h>
 
 #include <ScriptEngine.h>
-#include <scrEngine.h>
 
 #ifdef GTA_FIVE
 #define DEFAULT_APP_ID "382624125287399424"
@@ -82,7 +81,7 @@ static void UpdatePresence()
 
 		if (!g_richPresenceOverride.empty())
 		{
-			line1 = g_richPresenceOverride;
+			line2 = g_richPresenceOverride;
 		}
 
 		if (g_discordAppId != g_lastDiscordAppId) 
@@ -215,18 +214,18 @@ static InitFunction initFunction([]()
 
 	fx::ScriptEngine::RegisterNativeHandler("SET_RICH_PRESENCE", [](fx::ScriptContext& context)
 	{
-		const char* str = context.GetArgument<const char*>(0);
+		std::string newValue;
 
-		if (str)
+		if (const char* str = context.GetArgument<const char*>(0))
 		{
-			g_richPresenceOverride = str;
-		}
-		else
-		{
-			g_richPresenceOverride = "";
+			newValue = str;
 		}
 
-		g_richPresenceChanged = true;
+		if (g_richPresenceOverride != newValue)
+		{
+			g_richPresenceOverride = newValue;
+			g_richPresenceChanged = true;
+		}
 	});
 
 	fx::ScriptEngine::RegisterNativeHandler("SET_DISCORD_RICH_PRESENCE_ACTION", [](fx::ScriptContext& context)