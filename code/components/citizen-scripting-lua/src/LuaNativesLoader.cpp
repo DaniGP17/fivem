DaniGP17/fivem

commit f3010d29d7cba49f664d3c6cb1d89ca839d26369

fix(scripting/lua): don't double-load natives.zip
This was somehow loaded and mounted for every Lua component involved.

code/components/citizen-scripting-lua/src/LuaNativesLoader.cpp
@@ -40,11 +40,59 @@ static void MountNatives(const std::string& name)
 	}
 }
 
+class MarkerDevice : public vfs::Device
+{
+	// Inherited via Device
+	virtual THandle Open(const std::string& fileName, bool readOnly) override
+	{
+		if (fileName == "n:m:a:r:k:e:r" && readOnly)
+		{
+			return 1;
+		}
+
+		return vfs::Device::InvalidHandle;
+	}
+
+	virtual size_t Read(THandle handle, void* outBuffer, size_t size) override
+	{
+		return -1;
+	}
+
+	virtual size_t Seek(THandle handle, intptr_t offset, int seekType) override
+	{
+		return -1;
+	}
+
+	virtual bool Close(THandle handle) override
+	{
+		return true;
+	}
+
+	virtual THandle FindFirst(const std::string& folder, vfs::FindData* findData) override
+	{
+		return vfs::Device::InvalidHandle;
+	}
+
+	virtual bool FindNext(THandle handle, vfs::FindData* findData) override
+	{
+		return false;
+	}
+
+	virtual void FindClose(THandle handle) override
+	{
+	}
+};
+
 static InitFunction initFunction([]()
 {
 	fx::ResourceManager::OnInitializeInstance.Connect([](fx::ResourceManager*)
 	{
-		static bool mountedFiles = false;
+		bool mountedFiles = false;
+
+		if (auto device = vfs::GetDevice("nativesLua:/marker/"); device.GetRef())
+		{
+			mountedFiles = device->Open("n:m:a:r:k:e:r", true) != vfs::Device::InvalidHandle;
+		}
 
 		if (!mountedFiles)
 		{
@@ -60,7 +108,7 @@ static InitFunction initFunction([]()
 			MountNatives("natives_server");
 #endif
 
-			mountedFiles = true;
+			vfs::Mount(new MarkerDevice(), "nativesLua:/marker/");
 		}
 	});
 });

commit 45b26ad5d682ce09aec1dba1e2bbfb236d3f6bda

fix(scripting/lua): actually set mountedAnyNatives (+ OAL rename)
This is a regression since f3010d29d7cba49f664d3c6cb1d89ca839d26369 that
somehow wasn't noticed/reported for a while now, where in the internal
multi-component use case with these natives (or with Lua 5.4 for open
builds), lazy natives/OAL would not be enabled.

See Linden's report on the forums: https://forum.cfx.re/t/4885838

This also changes `use_fxv2_oal` to `use_experimental_fxv2_oal` because
during the 3-month period where OAL did nothing people may have wrongly
set this flag on a script that should not work with OAL, but did while
this bug was present.

code/components/citizen-scripting-lua/src/LuaNativesLoader.cpp
@@ -9,7 +9,7 @@ namespace fx
 bool mountedAnyNatives = false;
 }
 
-static void MountNatives(const std::string& name)
+static bool MountNatives(const std::string& name)
 {
 	static std::map<std::string, std::vector<uint8_t>> storedFiles;
 
@@ -34,10 +34,13 @@ static void MountNatives(const std::string& name)
 
 		if (device->OpenArchive(fmt::sprintf("memory:$%016llx,%d,0:%s", (uintptr_t)thisData, thisDataSize, name)))
 		{
-			fx::mountedAnyNatives = true;
 			vfs::Mount(device, fmt::sprintf("nativesLua:/%s/", name));
+
+			return true;
 		}
 	}
+
+	return false;
 }
 
 class MarkerDevice : public vfs::Device
@@ -96,19 +99,26 @@ static InitFunction initFunction([]()
 
 		if (!mountedFiles)
 		{
+			mountedFiles =
 #if defined(IS_RDR3)
-			MountNatives("rdr3_universal");
+			MountNatives("rdr3_universal") &&
 #elif defined(GTA_NY)
-			MountNatives("ny_universal");
+			MountNatives("ny_universal") &&
 #elif defined(GTA_FIVE)
-			MountNatives("natives_universal");
-			MountNatives("natives_21e43a33");
-			MountNatives("natives_0193d0af");
+			MountNatives("natives_universal") &&
+			MountNatives("natives_21e43a33") &&
+			MountNatives("natives_0193d0af") &&
 #elif defined(IS_FXSERVER)
-			MountNatives("natives_server");
+			MountNatives("natives_server") &&
 #endif
+			true;
 
-			vfs::Mount(new MarkerDevice(), "nativesLua:/marker/");
+			if (mountedFiles)
+			{
+				vfs::Mount(new MarkerDevice(), "nativesLua:/marker/");
+			}
 		}
+
+		fx::mountedAnyNatives = mountedFiles;
 	});
 });

code/components/citizen-scripting-lua/src/LuaScriptNatives.cpp
@@ -865,7 +865,7 @@ LUA_SCRIPT_LINKAGE int Lua_LoadNative(lua_State* L)
 
 		if (isCfxv2) // TODO/TEMPORARY: fxv2 oal is disabled by default for now
 		{
-			runtime->GetScriptHost2()->GetNumResourceMetaData("use_fxv2_oal", &isCfxv2);
+			runtime->GetScriptHost2()->GetNumResourceMetaData("use_experimental_fxv2_oal", &isCfxv2);
 		}
 
 		//#if !defined(GTA_FIVE) || (LUA_VERSION_NUM == 504)