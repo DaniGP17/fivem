DaniGP17/fivem

commit 9ed3e5e88aaa647701d25997fd51884311f3a75f

tweak(nui/core): ensure lastClickCount is non-zero
CEF will throw an assertion failure otherwise.

code/components/nui-core/src/CefInput.cpp
@@ -462,7 +462,7 @@ static HookFunction initFunction([] ()
 					((button == 0) ? MBT_LEFT : ((button == 1) ? MBT_RIGHT : MBT_MIDDLE));
 				if (!cancelPreviousClick && (btnType == lastClickButton))
 				{
-					++lastClickCount;
+					lastClickCount = std::min(3, lastClickCount + 1);
 				}
 				else
 				{
@@ -493,7 +493,7 @@ static HookFunction initFunction([] ()
 
 				auto browser = GetFocusBrowser();
 
-				if (browser)
+				if (browser && lastClickCount > 0)
 				{
 					CefMouseEvent mouse_event;
 					mouse_event.x = x;

commit ddf1f40acdcdb23eca318f8d6de5c11ffb6ba66a

fix(nui/core): send mouseUp
Regression caused by: 9ed3e5e88aaa647701d25997fd51884311f3a75f. The
mouse release event is required - just need to ensure the click-count is
non-zero.

code/components/nui-core/src/CefInput.cpp
@@ -495,13 +495,13 @@ static HookFunction initFunction([] ()
 
 				auto browser = GetFocusBrowser();
 
-				if (browser && lastClickCount > 0)
+				if (browser)
 				{
 					CefMouseEvent mouse_event;
 					mouse_event.x = x;
 					mouse_event.y = y;
 					mouse_event.modifiers = GetCefMouseModifiers();
-					browser->GetHost()->SendMouseClickEvent(mouse_event, btnType, true, lastClickCount);
+					browser->GetHost()->SendMouseClickEvent(mouse_event, btnType, true, std::max(1, lastClickCount));
 				}
 			}
 		}