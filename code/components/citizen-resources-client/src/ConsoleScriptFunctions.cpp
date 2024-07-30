DaniGP17/fivem

commit c470e5e2367e2265b7d5e7577792b687640aa927

refactor(convar): added flag to restrict convar read access by native

code/components/citizen-resources-client/src/ConsoleScriptFunctions.cpp
@@ -7,17 +7,11 @@ static InitFunction initFunction([]()
 {
 	fx::ScriptEngine::RegisterNativeHandler("GET_CONVAR", [](fx::ScriptContext& context)
 	{
-		// get variable name and validate it
-		std::string varName = context.CheckArgument<const char*>(0);
+		// get variable name
+		const std::string varName = context.CheckArgument<const char*>(0);
 
-		// this should not be exposed to script
-		// #TODO: 'not exposed to script' convar flags
-		if (HashString(varName.c_str()) == HashString("cl_ownershipTicket") || // ownership ticket is *very* bad to expose as users can pretend to be other users using this
-			HashString(varName.c_str()) == HashString("ui_updateChannel"))     // update channel is often misused as a marker for other things
-		{
-			context.SetResult(context.CheckArgument<const char*>(1));
-			return;
-		}
+		// get default value
+		const char* defaultValue = context.CheckArgument<const char*>(1);
 
 		// get the console context
 		auto consoleContext = console::GetDefaultContext();
@@ -25,47 +19,60 @@ static InitFunction initFunction([]()
 		// get the variable manager
 		auto varMan = consoleContext->GetVariableManager();
 
+		// check can it be exposed to script
+		if (varMan->GetEntryFlags(varName) & ConVar_ScriptRestricted)
+		{
+			context.SetResult(defaultValue);
+			return;
+		}
+
 		// get the variable
 		auto var = varMan->FindEntryRaw(varName);
 
 		if (!var)
 		{
-			context.SetResult(context.CheckArgument<const char*>(1));
+			context.SetResult(defaultValue);
+			return;
 		}
-		else
-		{
-			static std::string varVal;
-			varVal = var->GetValue();
 
-			context.SetResult(varVal.c_str());
-		}
+		static std::string varVal;
+		varVal = var->GetValue();
+
+		context.SetResult(varVal.c_str());
 	});
 
 	fx::ScriptEngine::RegisterNativeHandler("GET_CONVAR_INT", [](fx::ScriptContext& context)
 	{
+		const std::string varName = context.CheckArgument<const char*>(0);
+		const int defaultValue = context.GetArgument<int>(1);
+
 		// get the server's console context
 		auto consoleContext = console::GetDefaultContext();
 
 		// get the variable manager
 		auto varMan = consoleContext->GetVariableManager();
 
-		// get the variable
-		auto var = varMan->FindEntryRaw(context.CheckArgument<const char*>(0));
+		// check can it be exposed to script
+		if (varMan->GetEntryFlags(varName) & ConVar_ScriptRestricted)
+		{
+			context.SetResult(defaultValue);
+			return;
+		}
 
+		// get the variable
+		auto var = varMan->FindEntryRaw(varName);
 		if (!var)
 		{
-			context.SetResult(context.GetArgument<int>(1));
+			context.SetResult(defaultValue);
+			return;
 		}
-		else
+
+		auto value = var->GetValue();
+		if (value == "true")
 		{
-			auto value = var->GetValue();
-
-			if (value == "true")
-			{
-				value = "1";
-			}
-
-			context.SetResult(atoi(value.c_str()));
+			value = "1";
 		}
+
+		context.SetResult(atoi(value.c_str()));
 	});
 });

code/client/citicore/console/Console.Variables.h
@@ -73,6 +73,9 @@ enum ConsoleVariableFlags
 
 	// blocks any modifications to the convar via set(s|a|r) commands
 	ConVar_Internal = 0x40,
+
+	// can't be read by script API
+	ConVar_ScriptRestricted = 0x80,
 };
 
 inline std::string ConsoleFlagsToString(ConsoleVariableFlags flags)
@@ -92,6 +95,8 @@ inline std::string ConsoleFlagsToString(ConsoleVariableFlags flags)
 		value += "ReadOnly ";
 	if (flags & ConVar_Internal)
 		value += "Internal ";
+	if (flags & ConVar_ScriptRestricted)
+		value += "ScriptRestricted ";
 	
 	return value;
 }

code/components/glue/src/ConnectToNative.cpp
@@ -982,7 +982,8 @@ static InitFunction initFunction([] ()
 
 	static ConVar<bool> uiPremium("ui_premium", ConVar_None, false);
 
-	static ConVar<std::string> uiUpdateChannel("ui_updateChannel", ConVar_None, curChannel,
+	// ConVar_ScriptRestricted because update channel is often misused as a marker for other things
+	static ConVar<std::string> uiUpdateChannel("ui_updateChannel", ConVar_ScriptRestricted, curChannel,
 	[](internal::ConsoleVariableEntry<std::string>* convar)
 	{
 		if (convar->GetValue() != curChannel)

code/components/ros-patches-five/src/LegitimacyChecking.cpp
@@ -957,7 +957,8 @@ namespace ros
 
 void LoadOwnershipEarly()
 {
-	tokenVar = new ConVar<std::string>("cl_ownershipTicket", ConVar_None, "");
+	// ConVar_ScriptRestricted because ownership ticket is sensitive information. Should never be exposed to 3rd party
+	tokenVar = new ConVar<std::string>("cl_ownershipTicket", ConVar_ScriptRestricted, "");
 
 	if (!tokenVar->GetValue().empty())
 	{

commit 35f36680ec34c3efbdc637f76aef91d250a3b49c

tweak(convar): allow null default value
This fixes the regression caused in c470e5e2367e2265b7d5e7577792b687640aa927, because some scripts rely on this undefined behavior.

code/components/citizen-resources-client/src/ConsoleScriptFunctions.cpp
@@ -10,9 +10,6 @@ static InitFunction initFunction([]()
 		// get variable name
 		const std::string varName = context.CheckArgument<const char*>(0);
 
-		// get default value
-		const char* defaultValue = context.CheckArgument<const char*>(1);
-
 		// get the console context
 		auto consoleContext = console::GetDefaultContext();
 
@@ -22,7 +19,8 @@ static InitFunction initFunction([]()
 		// check can it be exposed to script
 		if (varMan->GetEntryFlags(varName) & ConVar_ScriptRestricted)
 		{
-			context.SetResult(defaultValue);
+			// gets and returns default value
+			context.SetResult(context.GetArgument<const char*>(1));
 			return;
 		}
 
@@ -31,7 +29,8 @@ static InitFunction initFunction([]()
 
 		if (!var)
 		{
-			context.SetResult(defaultValue);
+			// gets and returns default value
+			context.SetResult(context.GetArgument<const char*>(1));
 			return;
 		}
 