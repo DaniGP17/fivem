189e365f70116a2c5e4868a797cdf61e72bb1b29
Daniel GP <96537843+DaniGP17@users.noreply.github.com>
2025-03-26 15:45:21 +0100

    fix(gta-streaming-five): refactor pool sizes declarations

--- code/components/gta-streaming-five/src/SceneGraphPools.cpp
+++ code/components/gta-streaming-five/src/SceneGraphPools.cpp
@@ -7,7 +7,7 @@
 
 struct PatternPair;
 
-enum class eSceneGraphPool
+enum class eSceneGraphPool : size_t
 {
 	FW_ENTITY_CONTAINER = 0,
 	FW_FIXED_ENTITY_CONTAINER = 1,
@@ -17,6 +17,7 @@ enum class eSceneGraphPool
 	FW_INTERIOR_SCENE_GRAPH_NODE = 5,
 	FW_ROOM_SCENE_GRAPH_NODE = 6,
 	FW_PORTAL_SCENE_GRAPH_NODE = 7,
+	COUNT
 };
 
 struct SceneGraphPoolData
@@ -25,21 +26,39 @@ struct SceneGraphPoolData
 	int poolSize;
 };
 
-std::unordered_map<eSceneGraphPool, SceneGraphPoolData> pools = {
-    {eSceneGraphPool::FW_ENTITY_CONTAINER, {48, 800}},
-    {eSceneGraphPool::FW_FIXED_ENTITY_CONTAINER, {48, 2}}, // Max pool size is 64
-    {eSceneGraphPool::FW_SO_A_ENTITY_CONTAINER, {40, 1000}}, // This pool size will apply to FW_STREAMED_SCENE_GRAPH_NODE
-    {eSceneGraphPool::FW_EXTERIOR_SCENE_GRAPH_NODE, {80, 1}}, // This pool size can't be changed, due to asm inc instruction
-    {eSceneGraphPool::FW_STREAMED_SCENE_GRAPH_NODE, {64, 1000}}, // This pool size is going to be overwritten by FW_SO_A_ENTITY_CONTAINER
-    {eSceneGraphPool::FW_INTERIOR_SCENE_GRAPH_NODE, {32, 150}},
-    {eSceneGraphPool::FW_ROOM_SCENE_GRAPH_NODE, {48, 256}},
-    {eSceneGraphPool::FW_PORTAL_SCENE_GRAPH_NODE, {128, 800}}
+constexpr size_t POOL_COUNT = static_cast<size_t>(eSceneGraphPool::COUNT);
+constexpr SceneGraphPoolData pools[POOL_COUNT] = {
+	{48, 800},  // FW_ENTITY_CONTAINER
+	{48, 2},    // FW_FIXED_ENTITY_CONTAINER(Max pool size is 64)
+	{40, 1000}, // FW_SO_A_ENTITY_CONTAINER(This pool size will apply to FW_STREAMED_SCENE_GRAPH_NODE)
+	{80, 1},    // FW_EXTERIOR_SCENE_GRAPH_NODE(This pool size can't be changed, due to asm inc instruction)
+	{64, 1000}, // FW_STREAMED_SCENE_GRAPH_NODE (This pool size is going to be overwritten by FW_SO_A_ENTITY_CONTAINER)
+	{32, 150},  // FW_INTERIOR_SCENE_GRAPH_NODE
+	{48, 256},  // FW_ROOM_SCENE_GRAPH_NODE
+	{128, 800}  // FW_PORTAL_SCENE_GRAPH_NODE
 };
+
+constexpr int FW_ENTITY_POOL_SIZE = pools[static_cast<size_t>(eSceneGraphPool::FW_ENTITY_CONTAINER)].poolSize;
+constexpr int FW_FIXED_ENTITY_POOL_SIZE = pools[static_cast<size_t>(eSceneGraphPool::FW_FIXED_ENTITY_CONTAINER)].poolSize;
+constexpr int FW_SO_A_ENTITY_POOL_SIZE = pools[static_cast<size_t>(eSceneGraphPool::FW_SO_A_ENTITY_CONTAINER)].poolSize;
+constexpr int FW_EXTERIOR_POOL_SIZE = pools[static_cast<size_t>(eSceneGraphPool::FW_EXTERIOR_SCENE_GRAPH_NODE)].poolSize;
+constexpr int FW_STREAMED_POOL_SIZE = pools[static_cast<size_t>(eSceneGraphPool::FW_STREAMED_SCENE_GRAPH_NODE)].poolSize;
+constexpr int FW_INTERIOR_POOL_SIZE = pools[static_cast<size_t>(eSceneGraphPool::FW_INTERIOR_SCENE_GRAPH_NODE)].poolSize;
+constexpr int FW_ROOM_POOL_SIZE = pools[static_cast<size_t>(eSceneGraphPool::FW_ROOM_SCENE_GRAPH_NODE)].poolSize;
+constexpr int FW_PORTAL_POOL_SIZE = pools[static_cast<size_t>(eSceneGraphPool::FW_PORTAL_SCENE_GRAPH_NODE)].poolSize;
+
+constexpr int FIRST_STREAMED_SCENE_NODE_INDEX = FW_EXTERIOR_POOL_SIZE;
+constexpr int FIRST_INTERIOR_SCENE_NODE_INDEX = FIRST_STREAMED_SCENE_NODE_INDEX + FW_STREAMED_POOL_SIZE;
+constexpr int FIRST_ROOM_SCENE_NODE_INDEX = FIRST_INTERIOR_SCENE_NODE_INDEX + FW_INTERIOR_POOL_SIZE;
+constexpr int FIRST_PORTAL_SCENE_NODE_INDEX = FIRST_ROOM_SCENE_NODE_INDEX + FW_ROOM_POOL_SIZE;
+constexpr int MAX_SCENE_NODE_COUNT = FIRST_PORTAL_SCENE_NODE_INDEX + FW_PORTAL_POOL_SIZE;
+constexpr int STORED_SCREEN_QUAD_COUNT = MAX_SCENE_NODE_COUNT - FIRST_ROOM_SCENE_NODE_INDEX + 1;
 
 int CalculateTotalSize(bool storage) {
 	int total = 0;
-	for (const auto& pool : pools) {
-		total += storage ? (pool.second.poolSize * pool.second.classSize) : pool.second.poolSize;
+	for (size_t i = 0; i < POOL_COUNT; ++i) {
+		const auto& pool = pools[i];
+		total += storage ? (pool.poolSize * pool.classSize) : pool.poolSize;
 	}
 	return total;
 }
@@ -50,7 +69,7 @@ struct PatternPair
 	int offset;
 };
 
-std::unique_ptr<uint8_t[]> skyPortalFlagArray = std::make_unique<uint8_t[]>((pools[eSceneGraphPool::FW_PORTAL_SCENE_GRAPH_NODE].poolSize + 7) / 8);
+std::unique_ptr<uint8_t[]> skyPortalFlagArray = std::make_unique<uint8_t[]>((FW_PORTAL_POOL_SIZE + 7) / 8);
 
 namespace rage
 {
@@ -75,11 +94,6 @@ namespace rage
 	};
 }
 
-static int FIRST_PORTAL_SCENE_NODE_INDEX =
-	pools[eSceneGraphPool::FW_EXTERIOR_SCENE_GRAPH_NODE].poolSize +
-	pools[eSceneGraphPool::FW_STREAMED_SCENE_GRAPH_NODE].poolSize +
-	pools[eSceneGraphPool::FW_INTERIOR_SCENE_GRAPH_NODE].poolSize +
-	pools[eSceneGraphPool::FW_ROOM_SCENE_GRAPH_NODE].poolSize;
 
 static void AddSkyPortal(rage::fwScanResultsSkyPortals* self, rage::fwPortalSceneGraphNode* skyPortalNode)
 {
@@ -97,7 +111,7 @@ static void AddSkyPortal(rage::fwScanResultsSkyPortals* self, rage::fwPortalScen
 
 static HookFunction hookFunction([]()
 {
-	assert(pools[eSceneGraphPool::FW_FIXED_ENTITY_CONTAINER].poolSize <= 64 && "The pool size of FW_FIXED_ENTITY_CONTAINER can't be greater than 64");
+	assert(FW_FIXED_ENTITY_POOL_SIZE <= 64 && "The pool size of FW_FIXED_ENTITY_CONTAINER can't be greater than 64");
 	auto initPoolsLocation = hook::get_pattern("48 89 5C 24 ? 48 89 6C 24 ? 48 89 74 24 ? 57 41 54 41 55 41 56 41 57 48 83 EC ? BA ? ? ? ? B9"); // 48 8D B3 ? ? ? ? 48 8B FB
 	auto assignStorageLocation = hook::get_pattern("48 8B C1 48 89 0D ? ? ? ? 48 81 C1");
 	auto computeSceneNodeLocation = hook::get_pattern("4C 8B C1 0F B6 49 ? 85 C9");
@@ -105,10 +119,10 @@ static HookFunction hookFunction([]()
 	auto clearVisDataLocation = hook::get_pattern("40 53 48 83 EC ? 48 8B D9 48 8B 89 ? ? ? ? 33 D2 41 B8");
 
 	// Adjust the memory offsets of the allocated pools
-	hook::put<uint32_t>((char*)initPoolsLocation + 88, pools[eSceneGraphPool::FW_ENTITY_CONTAINER].poolSize); // 800
-	hook::put<uint32_t>((char*)initPoolsLocation + 98, pools[eSceneGraphPool::FW_ENTITY_CONTAINER].poolSize + pools[eSceneGraphPool::FW_FIXED_ENTITY_CONTAINER].poolSize); // 802
-	hook::put<uint32_t>((char*)initPoolsLocation + 81, pools[eSceneGraphPool::FW_SO_A_ENTITY_CONTAINER].poolSize); // 1802
-	hook::put<uint32_t>((char*)initPoolsLocation + 123, pools[eSceneGraphPool::FW_INTERIOR_SCENE_GRAPH_NODE].poolSize + pools[eSceneGraphPool::FW_ROOM_SCENE_GRAPH_NODE].poolSize); // 406
+	hook::put<uint32_t>((char*)initPoolsLocation + 88, FW_ENTITY_POOL_SIZE); // 800
+	hook::put<uint32_t>((char*)initPoolsLocation + 98, FW_ENTITY_POOL_SIZE + FW_FIXED_ENTITY_POOL_SIZE); // 802
+	hook::put<uint32_t>((char*)initPoolsLocation + 81, FW_SO_A_ENTITY_POOL_SIZE); // 1802
+	hook::put<uint32_t>((char*)initPoolsLocation + 123, FW_INTERIOR_POOL_SIZE + FW_ROOM_POOL_SIZE); // 406
 
 	const int poolStorageSize = CalculateTotalSize(true);
 	const int poolFlagSize = CalculateTotalSize(false);
@@ -118,44 +132,36 @@ static HookFunction hookFunction([]()
 	hook::put<uint32_t>((char*)initPoolsLocation + 34, poolStorageSize + poolFlagSize);
 
 	// Change the pool size of each pool constructor
-	hook::put<uint32_t>((char*)initPoolsLocation + 176, pools[eSceneGraphPool::FW_ENTITY_CONTAINER].poolSize);
+	hook::put<uint32_t>((char*)initPoolsLocation + 176, FW_ENTITY_POOL_SIZE);
 	/*
 	 * To apply the size in the constructor of FW_FIXED_ENTITY_CONTAINER we have to know that the value is being calculated based on an rdi calculation,
 	 * above you can see the instruction "mov edi, 40h" and the argument is passed as "lea edx, [rdi-X]",
 	 * we calculate this value X based on the pool size we want, by default the poolSize is 2, X is 3Eh because 64 - 2 = 62(3E)
 	 */
-	hook::put((char*)initPoolsLocation + 253, (uint8_t)(64 - pools[eSceneGraphPool::FW_FIXED_ENTITY_CONTAINER].poolSize));
-	hook::put((char*)initPoolsLocation + 336, pools[eSceneGraphPool::FW_SO_A_ENTITY_CONTAINER].poolSize);
+	hook::put((char*)initPoolsLocation + 253, (uint8_t)(64 - FW_FIXED_ENTITY_POOL_SIZE));
+	hook::put((char*)initPoolsLocation + 336, FW_SO_A_ENTITY_POOL_SIZE);
 	// We skip the pool size of FW_EXTERIOR_SCENE_GRAPH_NODE because it can't be changed due to the limitations in the memory offsets when allocating the pools
-	hook::put((char*)initPoolsLocation + 484, pools[eSceneGraphPool::FW_STREAMED_SCENE_GRAPH_NODE].poolSize);
-	hook::put((char*)initPoolsLocation + 563, pools[eSceneGraphPool::FW_INTERIOR_SCENE_GRAPH_NODE].poolSize);
-	hook::put((char*)initPoolsLocation + 646, pools[eSceneGraphPool::FW_ROOM_SCENE_GRAPH_NODE].poolSize);
-	hook::put((char*)initPoolsLocation + 725, pools[eSceneGraphPool::FW_PORTAL_SCENE_GRAPH_NODE].poolSize);
+	hook::put((char*)initPoolsLocation + 484, FW_STREAMED_POOL_SIZE);
+	hook::put((char*)initPoolsLocation + 563, FW_INTERIOR_POOL_SIZE);
+	hook::put((char*)initPoolsLocation + 646, FW_INTERIOR_POOL_SIZE);
+	hook::put((char*)initPoolsLocation + 725, FW_PORTAL_POOL_SIZE);
 
 	// Adjust offsets in AssignStorage function
-	hook::put<uint32_t>((char*)assignStorageLocation + 13, pools[eSceneGraphPool::FW_ENTITY_CONTAINER].poolSize * pools[eSceneGraphPool::FW_ENTITY_CONTAINER].classSize); // 38400
-	hook::put<uint8_t>((char*)assignStorageLocation + 27, static_cast<uint8_t>(pools[eSceneGraphPool::FW_FIXED_ENTITY_CONTAINER].poolSize * pools[eSceneGraphPool::FW_FIXED_ENTITY_CONTAINER].classSize)); // 96
-	hook::put<uint32_t>((char*)assignStorageLocation + 45, pools[eSceneGraphPool::FW_SO_A_ENTITY_CONTAINER].poolSize * pools[eSceneGraphPool::FW_SO_A_ENTITY_CONTAINER].classSize); // 40000
-	hook::put<uint8_t>((char*)assignStorageLocation + 59, static_cast<uint8_t>(pools[eSceneGraphPool::FW_EXTERIOR_SCENE_GRAPH_NODE].poolSize * pools[eSceneGraphPool::FW_EXTERIOR_SCENE_GRAPH_NODE].classSize)); // 80
-	hook::put<uint32_t>((char*)assignStorageLocation + 70, pools[eSceneGraphPool::FW_STREAMED_SCENE_GRAPH_NODE].poolSize * pools[eSceneGraphPool::FW_STREAMED_SCENE_GRAPH_NODE].classSize); // 64000
-	hook::put<uint32_t>((char*)assignStorageLocation + 84, pools[eSceneGraphPool::FW_INTERIOR_SCENE_GRAPH_NODE].poolSize * pools[eSceneGraphPool::FW_INTERIOR_SCENE_GRAPH_NODE].classSize); // 4800
-	hook::put<uint32_t>((char*)assignStorageLocation + 98, pools[eSceneGraphPool::FW_ROOM_SCENE_GRAPH_NODE].poolSize * pools[eSceneGraphPool::FW_ROOM_SCENE_GRAPH_NODE].classSize); // 12288
-	hook::put<uint32_t>((char*)assignStorageLocation + 113, pools[eSceneGraphPool::FW_PORTAL_SCENE_GRAPH_NODE].poolSize * pools[eSceneGraphPool::FW_PORTAL_SCENE_GRAPH_NODE].classSize); // 102400
+	hook::put<uint32_t>((char*)assignStorageLocation + 13, FW_ENTITY_POOL_SIZE * pools[static_cast<size_t>(eSceneGraphPool::FW_ENTITY_CONTAINER)].classSize); // 38400
+	hook::put<uint8_t>((char*)assignStorageLocation + 27, static_cast<uint8_t>(FW_FIXED_ENTITY_POOL_SIZE * pools[static_cast<size_t>(eSceneGraphPool::FW_FIXED_ENTITY_CONTAINER)].classSize)); // 96
+	hook::put<uint32_t>((char*)assignStorageLocation + 45, FW_SO_A_ENTITY_POOL_SIZE * pools[static_cast<size_t>(eSceneGraphPool::FW_SO_A_ENTITY_CONTAINER)].classSize); // 40000
+	hook::put<uint8_t>((char*)assignStorageLocation + 59, static_cast<uint8_t>(FW_EXTERIOR_POOL_SIZE * pools[static_cast<size_t>(eSceneGraphPool::FW_EXTERIOR_SCENE_GRAPH_NODE)].classSize)); // 80
+	hook::put<uint32_t>((char*)assignStorageLocation + 70, FW_STREAMED_POOL_SIZE * pools[static_cast<size_t>(eSceneGraphPool::FW_STREAMED_SCENE_GRAPH_NODE)].classSize); // 64000
+	hook::put<uint32_t>((char*)assignStorageLocation + 84, FW_INTERIOR_POOL_SIZE * pools[static_cast<size_t>(eSceneGraphPool::FW_INTERIOR_SCENE_GRAPH_NODE)].classSize); // 4800
+	hook::put<uint32_t>((char*)assignStorageLocation + 98, FW_ROOM_POOL_SIZE * pools[static_cast<size_t>(eSceneGraphPool::FW_ROOM_SCENE_GRAPH_NODE)].classSize); // 12288
+	hook::put<uint32_t>((char*)assignStorageLocation + 113, FW_PORTAL_POOL_SIZE * pools[static_cast<size_t>(eSceneGraphPool::FW_PORTAL_SCENE_GRAPH_NODE)].classSize); // 102400
 
 	// Adjust offsets in ComputeSceneNode function
-	hook::put<uint32_t>((char*)computeSceneNodeLocation + 50, pools[eSceneGraphPool::FW_EXTERIOR_SCENE_GRAPH_NODE].poolSize + pools[eSceneGraphPool::FW_STREAMED_SCENE_GRAPH_NODE].poolSize + pools[eSceneGraphPool::FW_INTERIOR_SCENE_GRAPH_NODE].poolSize + pools[eSceneGraphPool::FW_ROOM_SCENE_GRAPH_NODE].poolSize); // 1407
-	hook::put<uint32_t>((char*)computeSceneNodeLocation + 90, pools[eSceneGraphPool::FW_EXTERIOR_SCENE_GRAPH_NODE].poolSize + pools[eSceneGraphPool::FW_STREAMED_SCENE_GRAPH_NODE].poolSize + pools[eSceneGraphPool::FW_INTERIOR_SCENE_GRAPH_NODE].poolSize); // 1151
-	hook::put<uint32_t>((char*)computeSceneNodeLocation + 113, pools[eSceneGraphPool::FW_EXTERIOR_SCENE_GRAPH_NODE].poolSize + pools[eSceneGraphPool::FW_STREAMED_SCENE_GRAPH_NODE].poolSize); // 1001
+	hook::put<uint32_t>((char*)computeSceneNodeLocation + 50, FW_EXTERIOR_POOL_SIZE + FW_STREAMED_POOL_SIZE + FW_INTERIOR_POOL_SIZE + FW_ROOM_POOL_SIZE); // 1407
+	hook::put<uint32_t>((char*)computeSceneNodeLocation + 90, FW_EXTERIOR_POOL_SIZE + FW_STREAMED_POOL_SIZE + FW_INTERIOR_POOL_SIZE); // 1151
+	hook::put<uint32_t>((char*)computeSceneNodeLocation + 113, FW_EXTERIOR_POOL_SIZE + FW_STREAMED_POOL_SIZE); // 1001
 
 	// Adjust offsets in AssignScratchBuffer function
-	int firstRoomSceneNodeIndex =
-		pools[eSceneGraphPool::FW_EXTERIOR_SCENE_GRAPH_NODE].poolSize +
-		pools[eSceneGraphPool::FW_STREAMED_SCENE_GRAPH_NODE].poolSize +
-		pools[eSceneGraphPool::FW_INTERIOR_SCENE_GRAPH_NODE].poolSize;
-	int sceneNodeCount =
-		firstRoomSceneNodeIndex +
-		pools[eSceneGraphPool::FW_ROOM_SCENE_GRAPH_NODE].poolSize +
-		pools[eSceneGraphPool::FW_PORTAL_SCENE_GRAPH_NODE].poolSize;
 	std::initializer_list<int> assignScratchBufferSizes = {
 		160, 177, 194, 211, 228
 	};
@@ -165,11 +171,12 @@ static HookFunction hookFunction([]()
 		// - Upper 16 bits (bitSize)
 		// - Lower 16 bits (size)
 		// Result: (bitSize << 16) | size
-		hook::put<uint32_t>((char*)assignScratchBufferLocation + offset, (sceneNodeCount << 16 ) | (sceneNodeCount + 31) >> 5);
+		hook::put<uint32_t>((char*)assignScratchBufferLocation + offset, (MAX_SCENE_NODE_COUNT << 16 ) | (MAX_SCENE_NODE_COUNT + 31) >> 5);
 	}
-	hook::put<uint32_t>((char*)assignScratchBufferLocation + 28, sceneNodeCount * 4);
-	hook::put<uint32_t>((char*)assignScratchBufferLocation + 54, sceneNodeCount - firstRoomSceneNodeIndex + 1);
-	hook::put<uint32_t>((char*)clearVisDataLocation + 20, sceneNodeCount * 4);
+	hook::put<uint32_t>((char*)assignScratchBufferLocation + 28, MAX_SCENE_NODE_COUNT * 4);
+	hook::put<uint32_t>((char*)assignScratchBufferLocation + 54, STORED_SCREEN_QUAD_COUNT * 16);
+	hook::put<uint32_t>((char*)clearVisDataLocation + 20, MAX_SCENE_NODE_COUNT * 4);
+
 
 	// Change pool size of FIRST_ROOM_SCENE_NODE_INDEX
 	std::initializer_list<PatternPair> roomSceneNodeIndexLocations = {
@@ -180,9 +187,9 @@ static HookFunction hookFunction([]()
 	for(auto& entry : roomSceneNodeIndexLocations)
 	{
 		hook::put<uint32_t>(hook::get_pattern(entry.pattern, entry.offset),
-			pools[eSceneGraphPool::FW_EXTERIOR_SCENE_GRAPH_NODE].poolSize +
-			pools[eSceneGraphPool::FW_STREAMED_SCENE_GRAPH_NODE].poolSize +
-			pools[eSceneGraphPool::FW_INTERIOR_SCENE_GRAPH_NODE].poolSize - 1);
+			FW_EXTERIOR_POOL_SIZE +
+			FW_STREAMED_POOL_SIZE +
+			FW_INTERIOR_POOL_SIZE - 1);
 	}
 
 	// Patch AddSkyPortal
@@ -190,7 +197,7 @@ static HookFunction hookFunction([]()
 	hook::trampoline(addSkyPortalLocation, AddSkyPortal);
 
 	// Patch SetIsSkyPortalVisInterior
-	auto fwScanNodesRunLocation = hook::get_pattern("48 8B 8B ? ? ? ? 33 D2 44 89 89");
+	auto fwScanNodesRunPortalLocation = hook::get_pattern("48 8B 8B ? ? ? ? 33 D2 44 89 89");
 	static struct : jitasm::Frontend
 	{
 		intptr_t location;
@@ -204,7 +211,7 @@ static HookFunction hookFunction([]()
 			mov(rcx, qword_ptr[rbx+0xC38]);
 			xor(edx, edx);
 			mov(dword_ptr[rcx+0x378], r9d);
-			mov(r8d, static_cast<size_t>((pools[eSceneGraphPool::FW_PORTAL_SCENE_GRAPH_NODE].poolSize + 7) / 8));
+			mov(r8d, static_cast<size_t>((FW_PORTAL_POOL_SIZE + 7) / 8));
 			mov(rcx, reinterpret_cast<uintptr_t>(skyPortalFlagArray.get()));
 			mov(rax, (uintptr_t)memset);
 			call(rax);
@@ -213,9 +220,9 @@ static HookFunction hookFunction([]()
 			mov(rax, this->location);
 			jmp(rax);
 		}
-	} fwScanNodesRunStub;
+	} fwScanNodesRunPortalStub;
 	
-	hook::nop(fwScanNodesRunLocation, 39);
-	fwScanNodesRunStub.Init(reinterpret_cast<intptr_t>(fwScanNodesRunLocation));
-	hook::jump(fwScanNodesRunLocation, fwScanNodesRunStub.GetCode());
+	hook::nop(fwScanNodesRunPortalLocation, 39);
+	fwScanNodesRunPortalStub.Init(reinterpret_cast<intptr_t>(fwScanNodesRunPortalLocation));
+	hook::jump(fwScanNodesRunPortalLocation, fwScanNodesRunPortalStub.GetCode());
 });
